1e4d172e0ede6517509c5b929d40a1b98b1fa777
evan-poster <evan.poster@gmail.com>
2024-12-09 16:21:49 -0500

    Uploader warning fix for loop

--- react-frontend/src/Uploader.js
+++ react-frontend/src/Uploader.js
@@ -10,7 +10,7 @@ function Uploader() {
 	<div className="file-upload">
 		<h3>File Upload Section</h3>
 		<form action="fileupload.php" encType="multipart/form-data" method="post">
-			<label className="custom-uploader" for="file">Upload Your File</label><br />
+			<label className="custom-uploader" htmlFor="file">Upload Your File</label><br />
 			<input id="file" accept="image/jpeg,image/gif,image/png,application/pdf,image/x-eps" name="fileToUpload" type="file" /><br />
 			<button className="btn" name="submit" type="submit">Upload File</button>
 		</form>
